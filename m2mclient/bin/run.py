faa16a708b00c25bac448a6292fc0d12067f0c5c
Georg Guba <georgguba@gmail.com>
2017-08-09 10:27:25 +0200

    channel closure, token approval, http server error handling

--- m2mclient/bin/run.py
+++ m2mclient/bin/run.py
@@ -15,17 +15,17 @@ from m2mclient.m2mclient import M2MClient
 @click.option('--key-path', default='privkey.txt', help='Path to private key file.')
 @click.option(
     '--channel-manager-address',
-    default='0x1111111111111111111111111111111111111111',
+    default='0x94856f00a8097103c4b623ede4a240f934b1062f',
     help='Ethereum address of the channel manager contract.'
 )
 @click.option(
-    '--channel-manager-abi-path',
-    default='channel_manager_abi.json',
-    help='Path to a file containing the channel manager ABI.'
+    '--contract-abi-path',
+    default='../../contracts/build/contracts.json',
+    help='Path to a file containing the ABIs for the token and channel manager.'
 )
 @click.option(
     '--token-address',
-    default='0x2608096b2aebd85bc3c61444ca43dc136cd5ced8',
+    default='0x90b90fa4f747d93d11a1401569b73e96689e0ed2',
     help='Ethereum address of the token contract.'
 )
 def run(
@@ -36,7 +36,7 @@ def run(
         rpc_port,
         key_path,
         channel_manager_address,
-        channel_manager_abi_path,
+        contract_abi_path,
         token_address
 ):
     client = M2MClient(
@@ -47,12 +47,12 @@ def run(
         rpc_port,
         key_path,
         channel_manager_address,
-        channel_manager_abi_path,
+        contract_abi_path,
         token_address
     )
 
     # client.request_resource('myresource')
-    client.open_channel('0x1111111111111111111111111111111111111111', 200)
+    client.open_channel('0xd1bf222ef7289ae043b723939d86c8a91f3aac3f', 10)
 
 
 if __name__ == '__main__':
