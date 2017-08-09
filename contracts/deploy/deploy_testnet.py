63b7cda02af514c12c94e54b9c3ef1f54860ffe9
Loredana <loredana.cirstea@gmail.com>
2017-08-09 11:24:28 +0200

    Fixed some issues from last review, name changes

--- contracts/deploy/deploy_testnet.py
+++ contracts/deploy/deploy_testnet.py
@@ -7,6 +7,10 @@ from web3 import Web3
 from web3.utils.compat import (
     Timeout,
 )
+senders = [
+    '0xe2e429949e97f2e31cd82facd0a7ae38f65e2f38',
+    '0xd1bf222ef7289ae043b723939d86c8a91f3aac3f'
+]
 
 def check_succesful_tx(web3: Web3, txid: str, timeout=180) -> dict:
     """See if transaction went through (Solidity code did not throw).
@@ -35,7 +39,7 @@ def main():
         print("Web3 provider is", web3.currentProvider)
 
         token = chain.provider.get_contract_factory("RDNToken")
-        txhash = token.deploy(args=[1000, "RDNToken", 6, "RDN"])
+        txhash = token.deploy(args=[10000, "RDNToken", 6, "RDN"])
         receipt = check_succesful_tx(chain.web3, txhash, 250)
         token_addr = receipt["contractAddress"]
         print("RDNToken address is", token_addr)
@@ -46,19 +50,9 @@ def main():
         cf_address = receipt["contractAddress"]
         print("RaidenMicroTransferChannels address is", cf_address)
 
-        # approve 100 to contract
-        '''
-        token(token_addr).transact({"from": web3.eth.accounts[0]}).approve(cf_address, 90);
+        for sender in senders:
+            token(token_addr).transact({"from": web3.eth.accounts[1]}).transfer(sender, 400);
 
-        transfer_filter = channel_factory(cf_address).on('ChannelCreated')
-        txhash = channel_factory(cf_address).transact({"from": web3.eth.accounts[0]}).init(web3.eth.accounts[1], token_addr, 90, 10)
-        check_succesful_tx(web3, txhash)
-        wait(transfer_filter)
-        log_entries = transfer_filter.get()
-        print(log_entries[0]['args']['_sender'])
-        print(log_entries[0]['args']['_receiver'])
-        print(log_entries[0]['args']['_id'])
-        '''
 
 if __name__ == "__main__":
     main()
