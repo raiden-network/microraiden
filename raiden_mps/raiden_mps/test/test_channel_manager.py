ed26228c8bc4fe6aa02a485d34beeb2fa303d706
Jannik Luhn <jannik.luhn@brainbot.com>
2017-08-10 13:52:37 +0200

    testrpc setup for channel manager tests.

--- raiden_mps/raiden_mps/test/test_channel_manager.py
+++ raiden_mps/raiden_mps/test/test_channel_manager.py
@@ -1,40 +1,83 @@
+import json
+import os
 import pytest
+from populus.project import Project
+from populus.utils.wait import wait_for_transaction_receipt
+from populus.contracts.contract import construct_contract_factory
 from raiden_mps.channel_manager import ChannelManager
 
-CONTRACT_ADDRESS = '0xDFC2F3Adb77Be607C8C13e68fd0Cf9242b4925f7'.lower()
-RECEIVER_ADDRESS = ''
-SENDER_ADDRESS = ''
+
+test_dir = os.path.dirname(os.path.dirname(__file__))
+contracts_relative_path = 'data/contracts.json'
+compiled_contracts_path = os.path.join(test_dir, contracts_relative_path)
+compiled_contracts = json.load(open(compiled_contracts_path))
+
+
+def check_succesful_tx(web3, txid, timeout=180):
+    """See if transaction went through (Solidity code did not throw).
+    :return: Transaction receipt
+    """
+    receipt = wait_for_transaction_receipt(web3, txid, timeout=timeout)
+    txinfo = web3.eth.getTransaction(txid)
+    assert txinfo["gas"] != receipt["gasUsed"]
+    return receipt
+
+@pytest.fixture
+def sender():
+    return '0x82a978b3f5962a5b0957d9ee9eef472ee55b42f1'  # funded default account of testrpc
 
 
 @pytest.fixture
-def channel_manager():
-    class DummyBlockchain(object):
-        def set_channel_manager(self, channel_manager):
-            pass
-    channel_manager = ChannelManager(DummyBlockchain(), RECEIVER_ADDRESS)
-    return channel_manager
+def receiver():
+    return '0x7d577a597b2742b498cb5cf0c26cdcd726d39e6e'  # funded default account of testrpc
 
 
-def test_channel_opening(channel_manager):
-    assert SENDER_ADDRESS not in channel_manager.state.channels
-    channel_manager.event_channel_opened(SENDER_ADDRESS, 100)
-    assert SENDER_ADDRESS in channel_manager.state.channels
-    channel = channel_manager.state.channels[SENDER_ADDRESS]
-    assert channel.receiver == RECEIVER_ADDRESS
-    assert channel.sender == SENDER_ADDRESS
-    assert channel.deposit == 100
-    assert channel.balance == 0
-    assert channel.is_closed is False
+@pytest.fixture
+def chain():
+    project = Project()
+    project.is_compiled_contract_cache_stale = lambda: False
+    project.fill_contracts_cache(compiled_contracts, 0)
+    with project.get_chain('testrpc') as chain:
+        yield chain
 
 
-def test_undisputed_channel_close(channel_manager):
-    channel_manager.event_channel_opened(SENDER_ADDRESS, 100)
+@pytest.fixture
+def web3(chain):
+    return chain.web3
 
-    def fail(*args):
-        assert False
-    channel_manager.close_channel = fail
-    channel_manager.event_channel_close_requested(SENDER_ADDRESS, 0, 1000)
-    assert SENDER_ADDRESS in channel_manager.state.channels
-    channel = channel_manager.state.channels[SENDER_ADDRESS]
-    assert channel.settle_timeout == 1000
-    assert channel.is_closed is True
+
+@pytest.fixture
+def contracts(web3, chain, sender):
+    # deploy contracts
+    token_contract_data = compiled_contracts['RDNToken']
+    token_factory = web3.eth.contract(
+        contract_name='RDNToken',
+        abi=token_contract_data["abi"],
+        bytecode=token_contract_data["bytecode"],
+        bytecode_runtime=token_contract_data["bytecode_runtime"]
+    )
+    txhash = token_factory.deploy(args=[10000, "RDNToken", 6, "RDN"])
+    receipt = check_succesful_tx(chain.web3, txhash, 250)
+    token_addr = receipt["contractAddress"]
+    token_contract = token_factory(token_addr)
+
+    channel_contract_data = compiled_contracts['RaidenMicroTransferChannels']
+    channel_factory = web3.eth.contract(
+        contract_name='RDNToken',
+        abi=channel_contract_data["abi"],
+        bytecode=channel_contract_data["bytecode"],
+        bytecode_runtime=channel_contract_data["bytecode_runtime"]
+    )
+    txhash = channel_factory.deploy(args=[token_addr, 100])
+    receipt = check_succesful_tx(chain.web3, txhash, 250)
+    cf_address = receipt["contractAddress"]
+    channel_contract = channel_factory(cf_address)
+
+    # send RDNTokens to sender
+    token_contract.transact({"from": web3.eth.accounts[0]}).transfer(sender, 400)
+
+    return token_contract, channel_contract
+
+
+def test_channel_opening(contracts, sender, receiver):
+    pass
