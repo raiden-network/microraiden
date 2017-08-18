bf52e8a6f33fb1e4ddc9ea3776a0fd222f735703
Georg Guba <georg.guba@gmail.com>
2017-08-18 14:40:00 +0200

    tester utils only close channels matching receiving key

--- raiden_mps/raiden_mps/test/utils/client.py
+++ raiden_mps/raiden_mps/test/utils/client.py
@@ -1,6 +1,6 @@
 from raiden_mps.client.channel import Channel
 from raiden_mps.client.rmp_client import RMPClient
-from raiden_mps.crypto import sign_close
+from raiden_mps.crypto import sign_close, privkey_to_addr
 
 
 def close_all_channels(client: RMPClient):
@@ -9,12 +9,16 @@ def close_all_channels(client: RMPClient):
 
 
 def close_all_channels_cooperatively(client: RMPClient, privkey_receiver: str, balance=None):
-    channels = [c for c in client.channels if c.state != Channel.State.closed]
+    receiver_addr = privkey_to_addr(privkey_receiver)
+    channels = [
+        c for c in client.channels if c.state != Channel.State.closed and
+                                      c.receiver == receiver_addr
+    ]
     for channel in channels:
         if balance is not None:
             channel.balance = 0
-            channel.state = Channel.State.open # for settling channels
+            channel.state = Channel.State.open  # for settling channels
             channel.create_transfer(balance)
 
         closing_sig = sign_close(privkey_receiver, channel.balance_sig)
-        assert(channel.close_cooperatively(closing_sig))
+        assert (channel.close_cooperatively(closing_sig))
