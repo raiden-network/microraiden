c33a084ee8676b8436197cbc600fe61ba6f3e211
Loredana <loredana.cirstea@gmail.com>
2017-08-21 15:25:20 +0300

    Simple testnet deployment script - add `click` and docs

--- contracts/README.md
+++ contracts/README.md
@@ -47,13 +47,13 @@ populus compile
 pytest
 pytest tests/test_raidenchannels.py -p no:warnings -s
 
-# deploy
-python deploy/deploy_testnet.py
-python deploy/deploy_performancetest.py
-
 ```
 
-#### Deployment - chain setup
+### Deployment
+
+
+
+#### Chain setup
 
  * `kovan`
    - change default account: https://github.com/loredanacirstea/raiden-micropayment-service/blob/master/contracts/populus.json#L136
@@ -61,6 +61,23 @@ python deploy/deploy_performancetest.py
    ```
    parity --geth --chain kovan --force-ui --reseal-min-period 0 --jsonrpc-cors http://localhost --jsonrpc-apis web3,eth,net,parity,traces,rpc,personal --unlock 0x5601Ea8445A5d96EEeBF89A67C4199FbB7a43Fbb --password ~/password.txt --author 0x5601Ea8445A5d96EEeBF89A67C4199FbB7a43Fbb
    ```
+
+
+```sh
+
+# Fast deploy on kovan / ropsten / testrpc
+
+# Following two calls are quivalent
+python deploy/deploy_testnet.py
+python deploy/deploy_testnet.py --chain kovan --supply 100000 --token-name RDNToken --token-decimals 6 --token-symbol RDN --senders '0xe2e429949e97f2e31cd82facd0a7ae38f65e2f38,0xd1bf222ef7289ae043b723939d86c8a91f3aac3f,0xE0902284c85A9A03dAA3B5ab032e238cc05CFF9a,0x0052D7B657553E7f47239d8c4431Fef001A7f99c'
+
+# Provide a custom token
+python deploy/deploy_testnet.py --token-address address
+
+# Performance test
+python deploy/deploy_performancetest.py
+
+```
 
 
 ### Automatic docs creation
--- contracts/deploy/deploy_testnet.py
+++ contracts/deploy/deploy_testnet.py
@@ -1,60 +1,102 @@
-"""
+'''
 A simple Python script to deploy contracts and then do a smoke test for them.
-"""
+'''
+import click
 from populus import Project
 from populus.utils.wait import wait_for_transaction_receipt
 from web3 import Web3
-from web3.utils.compat import (
-    Timeout,
-)
-senders = [
-    '0xe2e429949e97f2e31cd82facd0a7ae38f65e2f38',
-    '0xd1bf222ef7289ae043b723939d86c8a91f3aac3f',
-    '0xE0902284c85A9A03dAA3B5ab032e238cc05CFF9a',
-    '0x0052D7B657553E7f47239d8c4431Fef001A7f99c'
-]
+
+import math
+
 
 def check_succesful_tx(web3: Web3, txid: str, timeout=180) -> dict:
-    """See if transaction went through (Solidity code did not throw).
+    '''See if transaction went through (Solidity code did not throw).
     :return: Transaction receipt
-    """
+    '''
     receipt = wait_for_transaction_receipt(web3, txid, timeout=timeout)
     txinfo = web3.eth.getTransaction(txid)
-    assert txinfo["gas"] != receipt["gasUsed"]
+    assert txinfo['gas'] != receipt['gasUsed']
     return receipt
 
 
-def wait(transfer_filter):
-    with Timeout(30) as timeout:
-        while not transfer_filter.get(False):
-            timeout.sleep(2)
-
-
-def main():
+@click.command()
+@click.option(
+    '--chain',
+    default='kovan',
+    help='Chain to deploy on: kovan | ropsten | testrpc'
+)
+@click.option(
+    '--challenge-period',
+    default=30,
+    help='Challenge period in number of blocks.'
+)
+@click.option(
+    '--supply',
+    default=10000000,
+    help='Token contract supply (number of total issued tokens).'
+)
+@click.option(
+    '--token-name',
+    default='RDNToken',
+    help='Token contract name.'
+)
+@click.option(
+    '--token-decimals',
+    default=6,
+    help='Token contract number of decimals.'
+)
+@click.option(
+    '--token-symbol',
+    default='RDN',
+    help='Token contract symbol.'
+)
+@click.option(
+    '--token-address',
+    help='Already deployed token address.'
+)
+@click.option(
+    '--senders',
+    default='0xe2e429949e97f2e31cd82facd0a7ae38f65e2f38,0xd1bf222ef7289ae043b723939d86c8a91f3aac3f,0xE0902284c85A9A03dAA3B5ab032e238cc05CFF9a,0x0052D7B657553E7f47239d8c4431Fef001A7f99c',
+    help='Sender addresses for assigning tokens.'
+)
+def main(**kwargs):
     project = Project()
-    # chain_name = "testrpc"
-    chain_name = "kovan"
+
+    # print(kwargs)
+    chain_name = kwargs['chain']
+    challenge_period = kwargs['challenge_period']
+    supply = kwargs['supply']
+    senders = kwargs['senders'].split(',')
+    token_name = kwargs['token_name']
+    token_decimals = kwargs['token_decimals']
+    token_symbol = kwargs['token_symbol']
+    token_address = kwargs['token_address']
+    token_assign = math.floor(supply / len(senders))
+
     print("Make sure {} chain is running, you can connect to it, or you'll get timeout".format(chain_name))
 
     with project.get_chain(chain_name) as chain:
         web3 = chain.web3
-        print("Web3 provider is", web3.currentProvider)
+        print('Web3 provider is', web3.currentProvider)
 
-        token = chain.provider.get_contract_factory("RDNToken")
-        txhash = token.deploy(args=[100000, "RDNToken", 6, "RDN"])
-        receipt = check_succesful_tx(chain.web3, txhash, 250)
-        token_addr = receipt["contractAddress"]
-        print("RDNToken address is", token_addr)
+        if not token_address:
+            token = chain.provider.get_contract_factory(token_name)
+            txhash = token.deploy(args=[supply, token_name, token_decimals, token_symbol])
+            receipt = check_succesful_tx(chain.web3, txhash, 250)
+            token_address = receipt['contractAddress']
+            print(token_name, ' address is', token_address)
 
-        channel_factory = chain.provider.get_contract_factory("RaidenMicroTransferChannels")
-        txhash = channel_factory.deploy(args=[token_addr, 30])
+        channel_factory = chain.provider.get_contract_factory('RaidenMicroTransferChannels')
+        txhash = channel_factory.deploy(args=[token_address, challenge_period])
         receipt = check_succesful_tx(chain.web3, txhash, 250)
-        cf_address = receipt["contractAddress"]
-        print("RaidenMicroTransferChannels address is", cf_address)
+        cf_address = receipt['contractAddress']
+        print('RaidenMicroTransferChannels address is', cf_address)
 
         for sender in senders:
-            token(token_addr).transact({"from": web3.eth.accounts[1]}).transfer(sender, 20000);
+            token(token_address).transact({'from': web3.eth.accounts[0]}).transfer(sender, token_assign);
+
+        print('Senders have each been issued', token_assign, ' tokens')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
