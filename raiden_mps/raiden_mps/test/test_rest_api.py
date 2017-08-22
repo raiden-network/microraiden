937e43135b66d906e8c0153a75454a49837d9b07
Jozef Henzl <popcorp86@gmail.com>
2017-08-22 11:00:48 +0200

    Fix of REST api test

--- raiden_mps/raiden_mps/test/test_rest_api.py
+++ raiden_mps/raiden_mps/test/test_rest_api.py
@@ -1,5 +1,7 @@
 import pytest # noqa
 
+from raiden_mps.config import API_PATH
+
 
 def test_resources(doggo_proxy):
     app = doggo_proxy.app
@@ -11,11 +13,8 @@ def test_resources(doggo_proxy):
     rv = tc.get("/doggo.jpg")
     assert rv.status_code == 402
 
-    rv = tc.get("/cm")
+    rv = tc.get(API_PATH + "/channels/")
     assert rv.status_code == 200
 
-    rv = tc.get("/cm/channels/")
-    assert rv.status_code == 200
-
-    rv = tc.get("/cm/admin")
+    rv = tc.get(API_PATH + "/admin")
     assert rv.status_code == 200
