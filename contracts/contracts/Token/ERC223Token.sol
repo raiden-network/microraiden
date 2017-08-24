d5d40a4d2ee94feef7bdeda88b9f2de694b67ea6
Loredana <loredana.cirstea@gmail.com>
2017-08-24 03:08:44 +0300

    Add high level description in the main Readme
    Closes https://github.com/loredanacirstea/raiden-micropayment-service/issues/47

--- contracts/contracts/Token/ERC223Token.sol
+++ contracts/contracts/Token/ERC223Token.sol
@@ -1,5 +1,5 @@
 /*
-This Token Contract implements the standard token functionality (https://github.com/ethereum/EIPs/issues/20) as well as the following OPTIONAL extras intended for use by humans.
+This Token Contract implements the standard token functionality (https://github.com/ethereum/EIPs/issues/20), the ERC223 functionality (https://github.com/ethereum/EIPs/issues/223) as well as the following OPTIONAL extras intended for use by humans.
 
 In other words. This is intended for deployment in something like a Token Factory or Mist wallet, and then used by humans.
 Imagine coins, currencies, shares, voting weight, etc.
@@ -7,7 +7,6 @@ Machine-based, rapid creation of many tokens would not necessarily need these ex
 
 1) Initial Finite Supply (upon creation one specifies how much is minted).
 2) In the absence of a token registry: Optional Decimal, Symbol & Name.
-3) Optional approveAndCall() functionality to notify a contract if an approval() has occurred.
 
 .*/
 
