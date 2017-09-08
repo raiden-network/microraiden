a14bb38e9fdb738c93af28ca63c2207606843761
Jozef Henzl <popcorp86@gmail.com>
2017-09-08 13:19:30 +0200

    proxy: wikipaydia example uses click init template

--- microraiden/microraiden/examples/wikipaydia.py
+++ microraiden/microraiden/examples/wikipaydia.py
@@ -1,32 +1,12 @@
 import click
-import os
-import sys
 
-if __package__ is None:
-    path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, path)
-    sys.path.insert(0, path + "/../")
-
-from web3 import HTTPProvider, Web3
-from microraiden.make_helpers import make_paywalled_proxy
-from microraiden import config
-from microraiden.crypto import privkey_to_addr
 from microraiden.proxy.content import (
     PaywalledProxyUrl
 )
+from microraiden.click_helpers import main, pass_app
 
 
-@click.command()
-@click.option(
-    '--channel-manager-address',
-    default=None,
-    help='Ethereum address of the channel manager contract.'
-)
-@click.option(
-    '--private-key',
-    default='b6b2c38265a298a5dd24aced04a4879e36b5cc1a4000f61279e188712656e946',
-    help='Private key of the proxy'
-)
+@main.command()
 @click.option(
     '--host',
     default='localhost',
@@ -37,49 +17,8 @@ from microraiden.proxy.content import (
     default=5000,
     help='Port of the proxy'
 )
-@click.option(
-    '--rpc-provider',
-    default='http://localhost:8545',
-    help='Ethereum RPC provider address'
-)
-@click.option(
-    '--state-file',
-    default=None,
-    help='Port of the proxy'
-)
-@click.option(
-    '--paywall-info',
-    default=os.path.abspath(os.path.join(os.path.dirname(__file__), '../webui')),
-    help='Directory where the paywall info is stored. '
-         'The directory shoud contain a index.html file with the payment info/webapp. '
-         'Content of the directory (js files, images..) is available on the "js/" endpoint.'
-)
-def main(
-    channel_manager_address,
-    private_key,
-    paywall_info,
-    state_file,
-    rpc_provider,
-    host,
-    port
-):
-    if os.path.isfile(private_key):
-        with open(private_key) as keyfile:
-            private_key = keyfile.readline()[:-1]
-
-    channel_manager_address = channel_manager_address or config.CHANNEL_MANAGER_ADDRESS
-    receiver_address = privkey_to_addr(private_key)
-    if not state_file:
-        state_file_name = "%s_%s.pkl" % (channel_manager_address[:10], receiver_address[:10])
-        app_dir = click.get_app_dir('microraiden')
-        if not os.path.exists(app_dir):
-            os.makedirs(app_dir)
-        state_file = os.path.join(app_dir, state_file_name)
-
-    config.paywall_html_dir = paywall_info
-    web3 = Web3(HTTPProvider(rpc_provider, request_kwargs={'timeout': 60}))
-    app = make_paywalled_proxy(private_key, state_file, web3=web3)
-
+@pass_app
+def start(app, host, port):
     app.add_content(PaywalledProxyUrl(".*", 1, "http://en.wikipedia.org/", [r"wiki/.*"]))
     app.run(host=host, port=port, debug=True)
     app.join()
