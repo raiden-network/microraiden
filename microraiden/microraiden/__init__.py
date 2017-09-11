bf8d620be4835a6708c8bc904d50e365077fd745
Jozef Henzl <popcorp86@gmail.com>
2017-09-11 10:53:53 +0200

    Fixed #77: SSL infinite recursion when using gevent
    Python3.6 compatibility fix

--- microraiden/microraiden/__init__.py
+++ microraiden/microraiden/__init__.py
@@ -1,3 +1,10 @@
+# to fix requests's SSL infinite recursion bug, SSL must be
+# monkeypatched first, and only then requests can be imported
+# the bug occurs with Python3.6, requests=2.8.14 and gevent==1.2.2
+from gevent import monkey
+monkey.patch_ssl()
+import requests
+
 from .client import (
     Client,
     HTTPClient,
