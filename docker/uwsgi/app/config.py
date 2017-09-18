8c84856ba153ea386988dfd313628afa044c6c75
Jozef Henzl <popcorp86@gmail.com>
2017-09-18 12:19:20 +0000

    docker: enabled letsencrypt for demo domain

--- docker/uwsgi/app/config.py
+++ docker/uwsgi/app/config.py
@@ -7,5 +7,5 @@ RECEIVER_ADDRESS = privkey_to_addr(PRIVATE_KEY)
 # host and port Parity/Geth serves RPC requests on
 RPC_PROVIDER = 'http://172.17.0.1:8180'
 # state file to store proxy state and balance proofs
-STATE_FILE = "/files/%s_%s.pkl" % (config.CHANNEL_MANAGER_ADDRESS[:10], RECEIVER_ADDRESS[:10])
+STATE_FILE = "/files/%s_%s.json" % (config.CHANNEL_MANAGER_ADDRESS[:10], RECEIVER_ADDRESS[:10])
 SLEEP_RELOAD = 2
