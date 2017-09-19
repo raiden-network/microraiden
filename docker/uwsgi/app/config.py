4ddfa6ef0e2611395240ca082252bc278df6ad96
Jozef Henzl <popcorp86@gmail.com>
2017-09-19 11:15:51 +0200

    removed docker proxy privkey + certs

--- docker/uwsgi/app/config.py
+++ docker/uwsgi/app/config.py
@@ -2,7 +2,7 @@ from microraiden import config
 from microraiden.crypto import privkey_to_addr
 
 # private key of the content provider
-PRIVATE_KEY = 'b6b2c38265a298a5dd24aced04a4879e36b5cc1a4000f61279e188712656e946'
+PRIVATE_KEY = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 RECEIVER_ADDRESS = privkey_to_addr(PRIVATE_KEY)
 # host and port Parity/Geth serves RPC requests on
 RPC_PROVIDER = 'http://172.17.0.1:8180'
