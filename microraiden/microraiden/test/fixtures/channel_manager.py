5c2925f4851a478b169fefcfb20b65552fac48de
Jozef Henzl <popcorp86@gmail.com>
2017-09-21 14:23:01 +0200

    tests: blockchain state is reverted on test teardown

--- microraiden/microraiden/test/fixtures/channel_manager.py
+++ microraiden/microraiden/test/fixtures/channel_manager.py
@@ -3,11 +3,6 @@ import types
 from microraiden.channel_manager import ChannelManager, Blockchain
 
 
-@pytest.fixture
-def channel_managers_count():
-    return 2
-
-
 def start_channel_manager(channel_manager, use_tester, mine_sync_event):
     if use_tester:
 
@@ -39,8 +34,12 @@ def start_channel_manager(channel_manager, use_tester, mine_sync_event):
 @pytest.fixture
 def channel_manager(web3, receiver_privkey, make_channel_manager_proxy, token_contract, use_tester,
                     mine_sync_event):
+    if use_tester:
+        snapshot_id = web3.testing.snapshot()
     contract_proxy = make_channel_manager_proxy(receiver_privkey)
     manager = ChannelManager(web3, contract_proxy, token_contract, receiver_privkey,
                              n_confirmations=5)
     start_channel_manager(manager, use_tester, mine_sync_event)
-    return manager
+    yield manager
+    if use_tester:
+        web3.testing.revert(snapshot_id)
