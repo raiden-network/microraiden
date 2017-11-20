21d4706bbc1a0919bc4fc35c7553c3bd610eef21
Lefteris Karapetsas <lefteris@refu.co>
2017-11-20 14:51:51 +0100

    Braces closing style
    Make the braces closing style follow the consistent K&R style we use in Raiden.

--- contracts/contracts/RaidenMicroTransferChannels.sol
+++ contracts/contracts/RaidenMicroTransferChannels.sol
@@ -443,8 +443,11 @@ contract RaidenMicroTransferChannels {
         constant
         returns (uint)
     {
-        if (a > b) return a;
-        else return b;
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
     /// @dev Internal function for getting the minimum between two numbers.
@@ -456,8 +459,11 @@ contract RaidenMicroTransferChannels {
         constant
         returns (uint)
     {
-        if (a < b) return a;
-        else return b;
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
     /// @dev Internal function for getting an address from tokenFallback data bytes.
@@ -560,8 +566,7 @@ contract RaidenMicroTransferChannels {
         bytes32 ret;
         if (v == 0) {
             ret = '0';
-        }
-        else {
+        } else {
              while (v > 0) {
                 ret = bytes32(uint(ret) / (2 ** 8));
                 ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
@@ -608,7 +613,10 @@ contract RaidenMicroTransferChannels {
         constant
         returns (byte c)
     {
-        if (b < 10) return byte(uint8(b) + 0x30);
-        else return byte(uint8(b) + 0x57);
+        if (b < 10) {
+            return byte(uint8(b) + 0x30);
+        } else {
+            return byte(uint8(b) + 0x57);
+        }
     }
 }
