7202a9bb75c766040a8c39a5d4893db2b0a7528f
André Vitor de Lima Matos <andre@brainbot.com>
2017-11-29 15:50:57 -0200

    Use flask static files, instead of static files resource

--- microraiden/microraiden/proxy/resources/__init__.py
+++ microraiden/microraiden/proxy/resources/__init__.py
@@ -11,7 +11,6 @@ from .login import (
     ChannelManagementLogin,
     ChannelManagementLogout,
 )
-from .staticfiles import StaticFilesServer
 
 __all__ = (
     Expensive,
@@ -23,5 +22,4 @@ __all__ = (
     ChannelManagementStats,
     ChannelManagementLogin,
     ChannelManagementLogout,
-    StaticFilesServer,
 )
