3d882fec733cf9ddf8034a757cde48cf0d20ab1e
Georg Guba <georg.guba@gmail.com>
2017-12-07 13:50:17 +0100

    rebase fix for insufficient proxy funds test

--- microraiden/microraiden/test/test_broke_proxy.py
+++ microraiden/microraiden/test/test_broke_proxy.py
@@ -11,7 +11,8 @@ def test_cheating_client(
         doggo_proxy,
         web3,
         default_http_client: DefaultHTTPClient,
-        wait_for_blocks
+        wait_for_blocks,
+        http_doggo_url: str,
 ):
     patch_on_http_response(default_http_client, abort_on=[502])
     balance = web3.eth.getBalance(doggo_proxy.channel_manager.receiver)
@@ -21,13 +22,13 @@ def test_cheating_client(
                               'to': FAUCET_ADDRESS,
                               'value': balance - 90000})
     wait_for_blocks(1)
-    default_http_client.run('doggo.jpg')
+    default_http_client.get(http_doggo_url)
     # proxy is expected to return 502 - it has no funds
     assert default_http_client.last_response.status_code == 502
     web3.eth.sendTransaction({'from': FAUCET_ADDRESS,
                               'to': doggo_proxy.channel_manager.receiver,
                               'value': balance})
     wait_for_blocks(1)
-    default_http_client.run('doggo.jpg')
+    default_http_client.get(http_doggo_url)
     # now it should proceed normally
     assert default_http_client.last_response.status_code == 200
