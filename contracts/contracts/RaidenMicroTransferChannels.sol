155fc92398f50e3f8337eae35ba27d8521b03b6f
Loredana <loredana.cirstea@gmail.com>
2017-12-12 12:33:11 +0100

    Add verifyClosingSignature, same eip712 format, with sender’s address
    Change the closing signature message to contain the channel data instead of the sender’s signature of the channel data.

--- contracts/contracts/RaidenMicroTransferChannels.sol
+++ contracts/contracts/RaidenMicroTransferChannels.sol
@@ -172,38 +172,14 @@ contract RaidenMicroTransferChannels {
         require(token.transferFrom(msg.sender, address(this), _added_deposit));
     }
 
-    /// @notice Function called when any of the parties wants to close the channel and settle.
-    /// Receiver needs a balance proof to immediately settle, sender triggers a challenge period.
+    /// @notice Function called by the sender, receiver or a delegate, with all the needed
+    /// signatures to close the channel and settle immediately.
     /// @param _receiver_address The address that receives tokens.
     /// @param _open_block_number The block number at which a channel between the
     /// sender and receiver was created.
     /// @param _balance The amount of tokens owed by the sender to the receiver.
     /// @param _balance_msg_sig The balance message signed by the sender.
-    function uncooperativeClose(
-        address _receiver_address,
-        uint32 _open_block_number,
-        uint192 _balance,
-        bytes _balance_msg_sig)
-        external
-    {
-        address sender = verifyBalanceProof(_receiver_address, _open_block_number, _balance, _balance_msg_sig);
-
-        if(msg.sender == _receiver_address) {
-            settleChannel(sender, _receiver_address, _open_block_number, _balance);
-        } else {
-            require(msg.sender == sender);
-            initChallengePeriod(_receiver_address, _open_block_number, _balance);
-        }
-    }
-
-    /// @notice Function called by the sender, when he has a closing signature from the receiver
-    /// and channel is closed immediately.
-    /// @param _receiver_address The address that receives tokens.
-    /// @param _open_block_number The block number at which a channel between the
-    /// sender and receiver was created.
-    /// @param _balance The amount of tokens owed by the sender to the receiver.
-    /// @param _balance_msg_sig The balance message signed by the sender.
-    /// @param _closing_sig The hash of the signed balance message, signed by the receiver.
+    /// @param _closing_sig The receiver's signed balance message, containing the sender's address.
     function cooperativeClose(
         address _receiver_address,
         uint32 _open_block_number,
@@ -212,16 +188,41 @@ contract RaidenMicroTransferChannels {
         bytes _closing_sig)
         external
     {
-        // Derive receiver address from signature
-        address receiver = ECVerify.ecverify(keccak256(_balance_msg_sig), _closing_sig);
-        require(receiver == _receiver_address);
-
         // Derive sender address from signed balance proof
         address sender = verifyBalanceProof(_receiver_address, _open_block_number, _balance, _balance_msg_sig);
-        require(msg.sender == sender);
 
+        // Derive receiver address from closing signature
+        address receiver = verifyClosingSignature(sender, _open_block_number, _balance, _closing_sig);
+        require(receiver == _receiver_address);
+
+        // Both signatures have been verified and the channel can be settled.
         settleChannel(sender, receiver, _open_block_number, _balance);
     }
+
+    /// @notice Sender requests the closing of the channel and starts the challenge period.
+    /// This can only happen once.
+    /// @param _receiver_address The address that receives tokens.
+    /// @param _open_block_number The block number at which a channel between
+    /// the sender and receiver was created.
+    /// @param _balance The amount of tokens owed by the sender to the receiver.
+    function uncooperativeClose(
+        address _receiver_address,
+        uint32 _open_block_number,
+        uint192 _balance)
+        external
+    {
+        bytes32 key = getKey(msg.sender, _receiver_address, _open_block_number);
+
+        require(closing_requests[key].settle_block_number == 0);
+        require(_balance <= channels[key].deposit);
+
+        // Mark channel as closed
+        closing_requests[key].settle_block_number = uint32(block.number) + challenge_period;
+        require(closing_requests[key].settle_block_number > block.number);
+        closing_requests[key].closing_balance = _balance;
+        ChannelCloseRequested(msg.sender, _receiver_address, _open_block_number, _balance);
+    }
+
 
     /// @notice Function called by the sender after the challenge period has ended, in order to
     /// settle and delete the channel, in case the receiver has not closed the channel himself.
@@ -304,6 +305,39 @@ contract RaidenMicroTransferChannels {
         return signer;
     }
 
+    /// @dev Returns the receiver address extracted from the closing signature.
+    /// Works with eth_signTypedData https://github.com/ethereum/EIPs/pull/712.
+    /// @param _sender_address The address that sends tokens.
+    /// @param _open_block_number The block number at which a channel between the
+    /// sender and receiver was created.
+    /// @param _balance The amount of tokens owed by the sender to the receiver.
+    /// @param _closing_sig The receiver's signed balance message, containing the sender's address.
+    /// @return Address of the closing signature signer.
+    function verifyClosingSignature(
+        address _sender_address,
+        uint32 _open_block_number,
+        uint192 _balance,
+        bytes _closing_sig)
+        public
+        view
+        returns (address)
+    {
+        // The variable names from below will be shown to the sender when signing
+        // the balance proof, so they have to be kept in sync with the Dapp client.
+        // The hashed strings should be kept in sync with this function's parameters
+        // (variable names and types).
+        // ! Note that EIP712 might change how hashing is done, triggering a
+        // new contract deployment with updated code.
+        bytes32 message_hash = keccak256(
+          keccak256('address sender', 'uint32 block_created', 'uint192 balance', 'address contract'),
+          keccak256(_sender_address, _open_block_number, _balance, address(this))
+        );
+
+        // Derive address from signature
+        address signer = ECVerify.ecverify(message_hash, _closing_sig);
+        return signer;
+    }
+
     /// @notice Returns the unique channel identifier used in the contract.
     /// @param _sender_address The address that sends tokens.
     /// @param _receiver_address The address that receives tokens.
@@ -371,30 +405,6 @@ contract RaidenMicroTransferChannels {
         channels[key].deposit += _added_deposit;
         assert(channels[key].deposit > _added_deposit);
         ChannelToppedUp(_sender_address, _receiver_address, _open_block_number, _added_deposit);
-    }
-
-
-    /// @dev Sender starts the challenge period; this can only happen once.
-    /// @param _receiver_address The address that receives tokens.
-    /// @param _open_block_number The block number at which a channel between
-    /// the sender and receiver was created.
-    /// @param _balance The amount of tokens owed by the sender to the receiver.
-    function initChallengePeriod(
-        address _receiver_address,
-        uint32 _open_block_number,
-        uint192 _balance)
-        private
-    {
-        bytes32 key = getKey(msg.sender, _receiver_address, _open_block_number);
-
-        require(closing_requests[key].settle_block_number == 0);
-        require(_balance <= channels[key].deposit);
-
-        // Mark channel as closed
-        closing_requests[key].settle_block_number = uint32(block.number) + challenge_period;
-        require(closing_requests[key].settle_block_number > block.number);
-        closing_requests[key].closing_balance = _balance;
-        ChannelCloseRequested(msg.sender, _receiver_address, _open_block_number, _balance);
     }
 
     /// @dev Deletes the channel and settles by transfering the balance to the receiver
