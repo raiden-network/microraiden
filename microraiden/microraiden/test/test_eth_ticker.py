e5336feeee9a752ecd7b353582acbb23eb668473
Georg Guba <georg.guba@gmail.com>
2017-12-13 14:03:09 +0100

    improved speed of the ETH ticker test

--- microraiden/microraiden/test/test_eth_ticker.py
+++ microraiden/microraiden/test/test_eth_ticker.py
@@ -1,8 +1,11 @@
 import pytest  # noqa: F401
+from _pytest.monkeypatch import MonkeyPatch
+from flask import jsonify
 
 from microraiden import DefaultHTTPClient
 from microraiden.examples.eth_ticker import ETHTickerClient, ETHTickerProxy
 from microraiden.proxy.paywalled_proxy import PaywalledProxy
+from microraiden.proxy.resources import PaywalledProxyUrl
 
 
 @pytest.mark.needs_xorg
@@ -11,20 +14,30 @@ def test_eth_ticker(
         default_http_client: DefaultHTTPClient,
         sender_privkey: str,
         receiver_privkey: str,
+        monkeypatch: MonkeyPatch
 ):
-    proxy = ETHTickerProxy(receiver_privkey, proxy=empty_proxy) # noqa
-    ticker = ETHTickerClient(sender_privkey, httpclient=default_http_client)
+    def get_patched(*args, **kwargs):
+        body = {
+            'mid': '682.435', 'bid': '682.18', 'ask': '682.69', 'last_price': '683.16',
+            'low': '532.97', 'high': '684.0', 'volume': '724266.25906224',
+            'timestamp': '1513167820.721733'
+        }
+        return jsonify(body)
+
+    monkeypatch.setattr(PaywalledProxyUrl, 'get', get_patched)
+
+    ETHTickerProxy(receiver_privkey, proxy=empty_proxy)
+    ticker = ETHTickerClient(sender_privkey, httpclient=default_http_client, poll_interval=0.5)
 
     def post():
         ticker.close()
 
-        # This test fails if ETH price is below 100 USD. But why even bother anymore if it does?
-        assert float(ticker.pricevar.get().split()[0]) > 100
+        assert ticker.pricevar.get() == '683.16 USD'
         client = default_http_client.client
         assert len(client.get_open_channels()) == 0
         ticker.success = True
 
     ticker.success = False
-    ticker.root.after(5000, post)
+    ticker.root.after(1500, post)
     ticker.run()
     assert ticker.success
