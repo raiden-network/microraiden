e9dbf3f51644f23b009157f5e3ce2dca6a2c1bf4
Loredana <loredana.cirstea@gmail.com>
2017-12-20 10:12:07 +0200

    Use eth_utils._same_address
    Also compares normalized and checksummed representations

--- contracts/tests/test_ecverify.py
+++ contracts/tests/test_ecverify.py
@@ -1,7 +1,12 @@
 import pytest
 from ethereum import tester
 from utils import sign
+<<<<<<< HEAD
 from tests.utils import balance_proof_hash, closing_message_hash
+=======
+from eth_utils import encode_hex, is_same_address
+from tests.utils import balance_proof_hash
+>>>>>>> Use eth_utils._same_address
 from tests.fixtures import (
     owner_index,
     owner,
@@ -37,7 +42,7 @@ def test_ecrecover_output(web3, ecverify_test_contract):
 
     balance_message_hash = balance_proof_hash(B, block, balance, ecverify_test_contract.address)
     balance_msg_sig, addr = sign.check(balance_message_hash, tester.k0)
-    assert addr == A
+    assert is_same_address(addr, A)
 
     r = bytes.fromhex('12d99ba7bd20ac17bac65bfd646146c1ddbeb607519db6e7935684334d891ed6')
     s = bytes.fromhex('5d4ea3a13697c1d506f7bdb8cd672b944e2053d6d6bd87d4aa512fdc29ed9ae4')
@@ -84,8 +89,8 @@ def test_sign(web3, ecverify_test_contract):
     balance_message_hash2 = balance_proof_hash(B, block, balance + 1000, ecverify_test_contract.address)
     signed_message, addr = sign.check(balance_message_hash, tester.k0)
     signed_message_false, addr1 = sign.check(balance_message_hash, tester.k1)
-    assert addr == A
-    assert addr1 == B
+    assert is_same_address(addr, A)
+    assert is_same_address(addr1, B)
     assert len(signed_message) == 65
     assert len(signed_message_false) == 65
 
@@ -114,25 +119,25 @@ def test_sign(web3, ecverify_test_contract):
         balance_message_hash2,
         signed_message
     )
-    assert verified_address != A
+    assert not is_same_address(verified_address, A)
 
     verified_address = ecverify_test_contract.call().verify(
         balance_message_hash,
         signed_message
     )
-    assert verified_address == A
+    assert is_same_address(verified_address, A)
 
     verified_address_false = ecverify_test_contract.call().verify(
         balance_message_hash,
         signed_message_false
     )
-    assert verified_address_false != A
-    assert verified_address_false == B
+    assert not is_same_address(verified_address_false, A)
+    assert is_same_address(verified_address_false, B)
 
-    signer = '0x5601ea8445a5d96eeebf89a67c4199fbb7a43fbb'
+    signer = '0x5601Ea8445A5d96EEeBF89A67C4199FbB7a43Fbb'
     value = 1000
     value2 = 2000
-    _address = '0x5601ea8445a5d96eeebf89a67c4199fbb7a43fbb'
+    _address = '0x5601Ea8445A5d96EEeBF89A67C4199FbB7a43Fbb'
 
     signed_message = '0x0adc437691e266072e9aa1763329062a6b2fa1d7f94034f1b1e691218fe9fd285f4f20132fa00230f591571a3575456bb382040e02e93ff0f32544907748a9821c'
     signed_message = bytes.fromhex(signed_message[2:])
@@ -142,7 +147,7 @@ def test_sign(web3, ecverify_test_contract):
         value2,
         signed_message
     )
-    assert verified_address == signer
+    assert is_same_address(verified_address, signer)
 
 
 def test_extract_balance_proof_signature(get_accounts, token_instance, uraiden_instance):
@@ -150,13 +155,13 @@ def test_extract_balance_proof_signature(get_accounts, token_instance, uraiden_i
     token = token_instance
     uraiden = uraiden_instance
 
-    receiver = '0x5601ea8445a5d96eeebf89a67c4199fbb7a43fbb'
+    receiver = '0x5601Ea8445A5d96EEeBF89A67C4199FbB7a43Fbb'
     block = 4804175
     balance = 22000000000000000000
 
     message_hash = balance_proof_hash(receiver, block, balance, uraiden.address)
     balance_msg_sig, signer = sign.check(message_hash, tester.k2)
-    assert signer == A
+    assert is_same_address(signer, A)
 
     signature_address = uraiden.call().extractBalanceProofSignature(
         receiver,
@@ -164,7 +169,7 @@ def test_extract_balance_proof_signature(get_accounts, token_instance, uraiden_i
         balance,
         balance_msg_sig
     )
-    assert signature_address == signer
+    assert is_same_address(signature_address, signer)
 
     # Wrong receiver
     signature_address = uraiden.call().extractBalanceProofSignature(
@@ -173,7 +178,7 @@ def test_extract_balance_proof_signature(get_accounts, token_instance, uraiden_i
         balance,
         balance_msg_sig
     )
-    assert signature_address != signer
+    assert not is_same_address(signature_address, signer)
 
     # Wrong block
     signature_address = uraiden.call().extractBalanceProofSignature(
@@ -182,7 +187,7 @@ def test_extract_balance_proof_signature(get_accounts, token_instance, uraiden_i
         balance,
         balance_msg_sig
     )
-    assert signature_address != signer
+    assert not is_same_address(signature_address, signer)
 
     # Wrong balance
     signature_address = uraiden.call().extractBalanceProofSignature(
@@ -191,6 +196,7 @@ def test_extract_balance_proof_signature(get_accounts, token_instance, uraiden_i
         20,
         balance_msg_sig
     )
+<<<<<<< HEAD
     assert signature_address != signer
 
 
@@ -241,3 +247,6 @@ def test_extract_closing_signature(get_accounts, token_instance, uraiden_instanc
         balance_msg_sig
     )
     assert signature_address != signer
+=======
+    assert not is_same_address(signature_address, signer)
+>>>>>>> Use eth_utils._same_address
